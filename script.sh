cc339a52f978cbede06398a877cc78ef13ffe147
ledinhduy <duyleitbka95@gmail.com>
2021-12-22 14:52:40 +0700

    add script.sh

---
+++ script.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+echo "COPY .extension for debug to .emacs.d starting"
+cp -R .extension ~/.emacs.d/
+echo "COPY .extension for debug to .emacs.d done"
+
+
+echo "Install package libtool-bin (requirement for vterm) starting"
+sudo apt install libtool-bin
+echo "Install package libtool-bin (requirement for vterm) done"
